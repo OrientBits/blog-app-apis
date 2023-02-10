ae37ca5138a822d5e2764dfb6d7efe7441680083
Ramshek <ramshekrana@gmail.com>
2023-02-10 22:27:17 +0530

    fib fixes in security

--- src/main/java/com/orientbits/blogappapis/config/OpenApiConfig.java
+++ src/main/java/com/orientbits/blogappapis/config/OpenApiConfig.java
@@ -1,6 +1,6 @@
 package com.orientbits.blogappapis.config;
 
-import io.swagger.v3.core.util.Constants;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.Collections;
 
 
@@ -17,23 +16,33 @@ public class OpenApiConfig {
 
     private static final String API_KEY = "apiKey";
 
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes(API_KEY,apiKeySecuritySchema())) // define the apiKey SecuritySchema
+//                .info(new Info().title("Blog API").description(
+//                        "RESTful services documentation with OpenAPI 3."))
+//                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY))); // then apply it. If you don't apply it will not be added to the header in cURL
+//    }
+
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(API_KEY,apiKeySecuritySchema())) // define the apiKey SecuritySchema
                 .info(new Info().title("Blog API").description(
                         "RESTful services documentation with OpenAPI 3."))
-                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY))); // then apply it. If you don't apply it will not be added to the header in cURL
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
-    public SecurityScheme apiKeySecuritySchema() {
-        return new SecurityScheme()
-                .name("Authorization") // authorisation-token
-                .description("Description about the TOKEN")
-                .in(SecurityScheme.In.HEADER)
-                .type(SecurityScheme.Type.APIKEY);
-    }
 
 
 }
